7f34bb13aa0da05fb68c7bbe503625a4d233516b
Annika Stahl <annika@bespintech.com>
2015-11-17 21:18:32 -0500

    disable ihealth refresh button, add notification

--- client/views/pages/general/dashboards.js
+++ client/views/pages/general/dashboards.js
@@ -161,9 +161,22 @@ Template.serverList.helpers({
   }
 });
 
+Template.dashboardsIhealth.onCreated(function() {
+   this.buttonStateDisabled = new ReactiveVar(false);
+});
+
+Template.dashboardsIhealth.helpers({
+
+});
+
 Template.dashboardsIhealth.events({
-  'click .ihealth': function (event) {
+  'click .ihealth': function (event, instance) {
     event.preventDefault();
+    instance.buttonStateDisabled.set(true);
+    setTimeout(function() {
+      instance.buttonStateDisabled.set(false);
+      }, 20000);
+    toastr.success('Updating iHealth in the background','Data will automatically update!')
     Meteor.call("ihealthUpdateData");
   }
 });
@@ -172,5 +185,8 @@ Template.dashboardsIhealth.helpers({
   getIhealth: function () {
     var result = Ihealth.find({ hidden: false });
     return result;
+  },
+  buttonStateDisabled: function() {
+    return Template.instance().buttonStateDisabled.get();
   }
 });
--- client/views/pages/miscellaneous/notification.js
+++ client/views/pages/miscellaneous/notification.js
@@ -83,4 +83,3 @@ Template.toastrNotification.events({
 
 
 });
-
