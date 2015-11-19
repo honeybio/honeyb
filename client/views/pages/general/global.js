dd4a1a448838898d7a3ebdf5b39d8a8f8236d807
Annika Stahl <annika@bespintech.com>
2015-11-19 08:44:19 -0500

    updating table format checkpoint

--- client/views/pages/general/global.js
+++ client/views/pages/general/global.js
@@ -29,8 +29,17 @@ Template.registerHelper('getThis', function(attr) {
   var tmpVar = Template.parentData(1);
   return tmpVar[attr];
 });
-
+Template.registerHelper('getSyncGroup', function(sync_id) {
+  var device = Gtmsyncgroups.findOne({_id: sync_id});
+  return synchronizationGroupName;
+});
 Template.registerHelper('getOnDevice', function(device_id) {
   var device = Devices.findOne({_id: device_id});
   return device.self.name;
+});
+Template.registerHelper('isOnActive', function(device_id) {
+  var device = Devices.findOne({_id: device_id});
+  if (device.self.failoverState == 'active') {
+    return true;
+  }
 });
