1844ba59b53214f9ee20d921fb45f6bd86e5f294
Annika Stahl <annika@bespintech.com>
2015-11-23 08:56:53 -0500

    throughput chart for 1.1 on device page

--- client/views/pages/device/devices.js
+++ client/views/pages/device/devices.js
@@ -69,6 +69,144 @@ Template.deviceDetails.events({
     Meteor.call("updateStatClick", device_id);
   }
 });
+
+Template.deviceNetworkChart.rendered = function() {
+  var self = this;
+  self.autorun(function () {
+    self.subscribe("db_statistics", function(){
+      Tracker.autorun(function(){
+        var myLabel = [];
+        var mgmtDataIn = [];
+        var mgmtDataOut = [];
+        var oneDotOneIn = [];
+        var oneDotOneOut = [];
+        var today = new Date();
+        var curMin = Math.floor((today.getHours() * 60 + today.getMinutes()) / 5) * 5;
+        today.setHours(0);
+        today.setMinutes(0);
+        today.setSeconds(0);
+        today.setMilliseconds(0);
+        var stats = Statistics.findOne({device: self.data._id, timestamp_day: today, type: 'interface'});
+        for (var i = 0; i < 36; i++) {
+          // var labelDate = new Date((now.getHours() * 60 + now.getMinutes()) - ((36 - i) * 5) );
+          // var minutes = Math.floor((now.getHours() * 60 + now.getMinutes()) / 5) * 5 - ((36 - i) * 5);
+          var minutes = curMin - ((36 - i) * 5);
+          if (stats.values[minutes] !== undefined) {
+            if (stats.values[minutes].throughput === undefined || stats.values[minutes].throughput === null) {
+              today.setHours(0);
+              today.setMinutes(0);
+              today.setMinutes(minutes);
+              tFormat = today.getHours() + ":" + (today.getMinutes()<10?'0':'') + today.getMinutes();
+              myLabel.push(tFormat);
+              mgmtDataIn.push('0');
+              mgmtDataOut.push('0');
+              oneDotOneIn.push('0');
+              oneDotOneOut.push('0');
+            } else {
+              today.setHours(0);
+              today.setMinutes(0);
+              today.setMinutes(minutes);
+              tFormat = today.getHours() + ":" + (today.getMinutes()<10?'0':'') + today.getMinutes();
+              myLabel.push(tFormat);
+              mgmtDataIn.push(stats.values[minutes].throughput.mgmt.bpsIn);
+              mgmtDataOut.push(stats.values[minutes].throughput.mgmt.bpsOut);
+              oneDotOneIn.push(stats.values[minutes].throughput['1-dot-1'].bpsIn);
+              oneDotOneOut.push(stats.values[minutes].throughput['1-dot-1'].bpsOut);
+            }
+          }
+        }
+        // chart data
+        var lineData = {
+          labels: myLabel,
+          datasets: [
+            {
+              label: "1.1 Port Bits Per Second In",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(220,220,220,1)",
+              data: oneDotOneIn
+            },
+            {
+              label: "1.1 Port Bits per Second Out",
+              fillColor: "rgba(26,179,148,0.5)",
+              strokeColor: "rgba(26,179,148,0.7)",
+              pointColor: "rgba(26,179,148,1)",
+              pointStrokeColor: "#fff",
+              pointHighlightFill: "#fff",
+              pointHighlightStroke: "rgba(26,179,148,1)",
+              data: oneDotOneOut
+            }
+          ]
+        };
+
+        var lineOptions = {
+            scaleShowGridLines: true,
+            scaleShowLabels: true,
+            scaleGridLineColor: "rgba(0,0,0,.05)",
+            scaleGridLineWidth: 1,
+            bezierCurve: true,
+            bezierCurveTension: 0.4,
+            pointDot: true,
+            pointDotRadius: 4,
+            pointDotStrokeWidth: 1,
+            pointHitDetectionRadius: 20,
+            datasetStroke: true,
+            datasetStrokeWidth: 2,
+            datasetFill: true,
+            responsive: true,
+        };
+        var ctx = document.getElementById("lineChart").getContext("2d");
+        var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
+      });
+    });
+  });
+}
+
+Template.deviceDetails.rendered = function() {
+  var donutData = [
+    {
+      value: this.data.cpuUsage.fiveMinAvgIdle,
+      color: "#a3e1d4",
+      highlight: "#1ab394",
+      label: "Idle"
+    },
+    {
+      value: this.data.cpuUsage.fiveMinAvgIowait,
+      color: "#dedede",
+      highlight: "#1ab394",
+      label: "Disk Wait Time"
+    },
+    {
+      value: this.data.cpuUsage.fiveMinAvgSystem,
+      color: "#b5b8cf",
+      highlight: "#1ab394",
+      label: "System Processes"
+    },
+    {
+      value: this.data.cpuUsage.fiveMinAvgUser,
+      color: "#b5b8cf",
+      highlight: "#1ab394",
+      label: "User Processes"
+    }
+  ];
+  var donutOptions = {
+    segmentShowStroke: true,
+    segmentStrokeColor: "#fff",
+    segmentStrokeWidth: 2,
+    percentageInnerCutout: 45, // This is 0 for Pie charts
+    animationSteps: 100,
+    animationEasing: "easeOutBounce",
+    animateRotate: true,
+    animateScale: false,
+    responsive: true,
+  };
+  var ctx = document.getElementById("donutChart").getContext("2d");
+  var myNewChart = new Chart(ctx).Doughnut(donutData, donutOptions);
+};
+
 Template.deviceDetails.helpers({
   getJobs: function (device_id) {
     var results = Jobs.find({onDevice: device_id});
