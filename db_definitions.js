572a26bb285f67550728d75be87433f80224662f
Annika Stahl <annika@bespintech.com>
2015-12-01 08:55:33 -0500

    clean up files

--- db_definitions.js
+++ db_definitions.js
@@ -1,20 +1,3 @@
-
-/*
-Softwareimages = new FileCollection('fc_softwareimages',
-  { resumable: true,   // Enable built-in resumable.js upload support
-    chunkSize: 20*1024*1024 - 1024,
-    http: [
-      { method: 'get',
-        path: '/:md5',  // this will be at route "/gridfs/myFiles/:md5"
-        lookup: function (params, query) {  // uses express style url params
-          return { md5: params.md5 };       // a query mapping url to myFiles
-        }
-      }
-    ]
-  }
-);
-*/
-
 if (Meteor.isServer) {
   Archives.allow({
     insert: function(){
