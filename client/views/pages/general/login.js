9b3f5ace195a9f8dbbeb77ac4c69c458f5f631e2
Annika Stahl <annika@bespintech.com>
2015-12-02 21:15:25 -0500

    settings display click anywhere expand

--- client/views/pages/general/login.js
+++ client/views/pages/general/login.js
@@ -199,7 +199,20 @@ Template.settingsHoneyb.events({
   },
   'change #authenticationType': function(event, target) {
     Session.set('auth_type', authenticationType.options[authenticationType.selectedIndex].value);
-  }
+  },
+  'click .ibox-title': function (event) {
+      var element = $(event.target);
+      var ibox = element.closest('div.ibox');
+      var button = element.closest("i");
+      var content = ibox.find('div.ibox-content');
+      content.slideToggle(200);
+      button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+      ibox.toggleClass('').toggleClass('border-bottom');
+      setTimeout(function () {
+          ibox.resize();
+          ibox.find('[id^=map-]').resize();
+      }, 50);
+  },
 });
 
 Template.settingsRoles.helpers({
