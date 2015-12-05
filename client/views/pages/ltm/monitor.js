94c2e2c09f5cfe49518e4e7d73e9e985790a9900
Annika Stahl <annika@bespintech.com>
2015-12-05 11:34:57 -0500

    removed monitordetails myprops function

--- client/views/pages/ltm/monitor.js
+++ client/views/pages/ltm/monitor.js
@@ -104,25 +104,3 @@ Template.ltmMonitorsHttpCreate.events({
     });
   }
 });
-
-Template.monitorDetails.helpers('myProps', function() {
-  var attrList = [];
-  for (var attr in this) {
-    switch (attr) {
-      case "_id":
-        break;
-      case "onDevice":
-        break;
-      case "generation":
-        break;
-      case "selfLink":
-        break;
-      case "kind":
-        break;
-      default:
-        attrList.push(attr);
-    }
-  }
-  //console.log(attrList);
-  return attrList;
-});
